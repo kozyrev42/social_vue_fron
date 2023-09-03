4d657af356d1cbe241edca855ad48bb2aaac7002
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-09-03 17:21:23 +0700

    6) Сделал форму для создания Поста + выбора изображения;

--- src/components/User/UserPersonal.vue
+++ src/components/User/UserPersonal.vue
@@ -1,12 +1,108 @@
 <template>
-  <div class="w-96 mx-auto ">
-    Personal компонент
-  </div>
+    <div class="w-96 mx-auto ">
+        
+        <div>
+            <div>
+                <input v-model="title" class="w-96 mb-3 rounded-3xl border p-3 border-slate-300" type="text"
+                       placeholder="заголовок">
+            </div>
+            <div>
+                <textarea v-model="content" class="w-96 mb-3 rounded-3xl border p-3 border-slate-300"
+                          placeholder="контент"></textarea>
+            </div>
+            
+            <div class="flex mb-3 items-center">
+                <div>
+                    <!-- После выбора файла, срабатывает событие change, и вызывается метод storeImage -->
+                    <input @change="storeImage" ref="file" type="file" class="hidden"/>
+    
+                    <!-- При клике на ссылку, открытие диалогового окна выбора файла -->
+                    <a href="#" class="block p-2 w-32 text-center text-sm rounded-2xl bg-sky-500 text-white
+                    hover:bg-white hover:border hover:border-sky-500 hover:text-sky-500 box-border"
+                       @click.prevent="selectFile()">
+                        Изображение
+                    </a>
+                </div>
+                
+                <div>
+                    <!-- .prevent - чтобы ссылка "#" не дёргалась -->
+                    <a @click.prevent="image = null" href="#" class="ml-3">
+                        Отмена
+                    </a>
+                </div>
+            </div>
+            
+            <div v-if="image" class="mb-3">
+                <!-- браузер загрузит и отобразит изображение напрямую с сервера -->
+                <img class="rounded-2xl" :src="image.url" alt="preview">
+            </div>
+            
+            <div>
+                <a @click.prevent="createPost" href="#" class="block p-2 w-32 text-center rounded-2xl bg-green-600 text-white
+          hover:bg-white hover:border hover:border-green-600 hover:text-green-600 box-border ml-auto">
+                    Опубликовать
+                </a>
+            </div>
+        </div>
+    
+    </div>
 </template>
 
 <script>
+import api from "../../api.js";
+
 export default {
-  name: "UserPersonal"
+    name: "UserPersonal",
+    
+    data() {
+        return {
+            title: '',
+            content: '',
+            image: null
+        }
+    },
+    
+    methods: {
+        createPost() {
+            const image_id = this.image ? this.image.id : null
+            api.post('/api/post/create', {title: this.title, content: this.content, image_id: image_id})
+                .then(response => {
+                    this.title = ''
+                    this.content = ''
+                    this.image = null
+                    console.log(response)
+                })
+        },
+        
+        // цель этого метода - это обеспечить пользователю возможность выбора файла,
+        // не делая на самом деле видимым элемент <input type="file">.
+        selectFile() {
+            // получаем доступ к скрытому элементу
+            this.fileInput = this.$refs.file
+            
+            // события клика на элементе <input ref="file" type="file">
+            // открывается стандартное диалоговое окно для выбора файла
+            this.fileInput.click()
+        },
+        
+        storeImage(e) {
+            // получаем выбранный пользователем файл
+            const file = e.target.files[0]
+            
+            // создаем новый объект FormData, который позволяет отправить файлы через AJAX-запрос.
+            const formData = new FormData()
+            
+            // добавляем выбранный файл в объект FormData под именем image.
+            formData.append('image', file)
+            
+            // отправляет файл на сервер
+            api.post('/api/post/image', formData)
+                .then(response => {
+                    // сохраняете данные ответа в переменную "image"
+                    this.image = response.data.data
+                })
+        }
+    }
 }
 </script>
 
