7321f85ec3b05cfdac31bdc6c28de2995efa666e
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-09-11 20:01:22 +0700

    10. Кнопка 'Отписаться/Подписаться', отправка на сервер ID, для смены статуса

--- src/components/User/UserIndex.vue
+++ src/components/User/UserIndex.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="w-96 mx-auto">
     <div v-if="users">
-        <div v-for="user in users" class="mb-6 pb-6 border-b border-gray-400">
+        <div v-for="user in users" class="flex justify-between mb-6 pb-6 border-b border-gray-400">
             <router-link :to="{name: 'user.show', params: {id: user.id}}">
                 <p>{{user.name}}</p>
                 <p>{{user.email}}</p>
             </router-link>
+            
+            <div>
+                <a @click.prevent="toggleFollowing(user)"
+                   href="#"
+                   class="block p-2 w-32 text-center text-sm rounded-2xl"
+                   :class="user.is_followed ? 'border border-sky-500 bg-white text-sky-500' : 'bg-sky-500 text-white'">
+                    {{ user.is_followed ? 'Отписаться' : 'Подписаться' }}
+                </a>
+
+            </div>
         </div>
     </div>
   </div>
@@ -33,7 +43,14 @@ export default {
           .then(response => {
               this.users = response.data.data
           })
-      }
+      },
+        
+        toggleFollowing(user) {
+            api.get(`/api/users/${user.id}/toggle_following`)
+                .then(response => {
+                    user.is_followed = response.data.is_followed
+                })
+        }
   }
 }
 </script>
